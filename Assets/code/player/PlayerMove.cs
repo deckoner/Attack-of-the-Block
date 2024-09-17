93be0076650bd78ce0048b780c93c1ff52e98d0f
Deckoner <deckonerdev@gmail.com>
2024-09-17 23:43:51 +0200

    Finalizado

--- Assets/code/player/PlayerMove.cs
+++ Assets/code/player/PlayerMove.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        Vector2 MousePos = Input.mousePosition;
-        Debug.Log(MousePos)
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Posicion de raton en pantalla
+        Vector2 posicionRaton = Input.mousePosition;
+
+        // Convertir la posición del ratón a cordenadas en el juego
+        posicionRaton = Camera.main.ScreenToWorldPoint(posicionRaton);
+
+        // Actualizar la posición del objeto usando Rigidbody
+        rb.MovePosition(posicionRaton);
     }
 }
