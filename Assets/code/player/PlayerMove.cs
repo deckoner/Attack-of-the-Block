7b8da8df0fd4e7f86f4824250c303edd21c9a897
deckoner <deckonerDev@gmail.com>
2024-09-17 20:28:23 +0200

    Posicion de mouse debug

--- Assets/code/player/PlayerMove.cs
+++ Assets/code/player/PlayerMove.cs
@@ -7,7 +7,8 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector2 MousePos = Input.mousePosition;
+        Debug.Log(MousePos)
     }
 
     // Update is called once per frame
