aa71e7ac118b2ca3a834bdc974d44bd89622d905
Deckoner <deckonerDev@gmail.com>
2024-09-17 23:56:09 +0200

    Revert "Finalizado"
    This reverts commit 93be0076650bd78ce0048b780c93c1ff52e98d0f.

--- Assets/code/player/PlayerMove.cs
+++ Assets/code/player/PlayerMove.cs
@@ -4,24 +4,16 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    private Rigidbody2D rb;
-
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        Vector2 MousePos = Input.mousePosition;
+        Debug.Log(MousePos)
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Posicion de raton en pantalla
-        Vector2 posicionRaton = Input.mousePosition;
-
-        // Convertir la posición del ratón a cordenadas en el juego
-        posicionRaton = Camera.main.ScreenToWorldPoint(posicionRaton);
-
-        // Actualizar la posición del objeto usando Rigidbody
-        rb.MovePosition(posicionRaton);
+        
     }
 }
